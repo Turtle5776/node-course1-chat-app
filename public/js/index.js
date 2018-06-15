3f30634fbbbd3ae1aecc5d9291e38f65d53ece0a
daniel <dandeffiong21@gmail.com>
2018-06-15 01:33:05 +0100

    Emit newMessage on createMessage

--- public/js/index.js
+++ public/js/index.js
@@ -4,11 +4,6 @@ var socket = io();
   		console.log('Connected to server');
 });
 
-  		socket.emit('createMessage', {
-  			from: 'daniel',
-  			to: 'arnold',
-  			text: 'Learn your sustentacular tali'
-  	});
        
        socket.on('newMessage', function(message) {
        	 console.log('New Message', message);
--- server/server.js
+++ server/server.js
@@ -17,17 +17,14 @@ var io = socketIO(server);
 io.on('connection', (socket) => {
       console.log('New user connected');
 
-      
-
-      socket.emit('newMessage', {
-      	 from:'arnold',
-         to: 'daniel',
-         text: 'How far?',
-         createdAt: 124 
-      });
 
       socket.on('createMessage', (message) => {
       	console.log('createMessage', message);
+      	io.emit('newMessage', {
+      		from: message.from,
+            text: message.text,
+            createdAt: new Date().getTime()
+      	});
       });
 
    
